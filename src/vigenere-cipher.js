504d0f7c52fb55a0c925bfeeba6a930aa120cdb1
Kris Casper <sw.darkwind@gmail.com>
2020-09-28 12:05:27 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,57 @@
-const CustomError = require("../extensions/custom-error");
+const A_CODE = 65
+const Z_CODE = 90
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isPlain = true) {
+    this._isPlain = isPlain
+  }
+
+  _getFullKey(key, length) {
+    return key.toUpperCase().repeat(Math.ceil(length / key.length)).substring(0,length)
+  }
+
+  encrypt(message, key) {
+    if(typeof(message) == 'undefined' || typeof(key) == 'undefined') throw 'invalid arguments'
+    
+    key = this._getFullKey(key, message.length)
+    message = message.toUpperCase()
+
+    const encryptedChars = []
+    let j = 0
+
+    for(let i = 0; i < message.length; i++) {
+      let charCode = message.charCodeAt(i)
+      if(charCode >= A_CODE && charCode <= Z_CODE) {
+        encryptedChars.push( A_CODE + (charCode + key.charCodeAt(j)) % 26)
+        j++
+      }
+      else encryptedChars.push(charCode)
+    }
+
+    if(this._isPlain) return String.fromCharCode(...encryptedChars)
+    else return String.fromCharCode(...encryptedChars.reverse())
+  }
+   
+  decrypt(message, key) {
+    if(typeof(message) == 'undefined' || typeof(key) == 'undefined') throw 'invalid arguments'
+    
+    key = this._getFullKey(key, message.length)
+    message = message.toUpperCase()
+
+    const decryptedChars = []
+    let j = 0
+
+    for(let i = 0; i < message.length; i++) {
+      let charCode = message.charCodeAt(i)
+      if(charCode >= A_CODE && charCode <= Z_CODE) {
+        decryptedChars.push( A_CODE + (charCode - key.charCodeAt(j) + 26) % 26)
+        j++
+      }
+      else decryptedChars.push(charCode)
+    }
+
+    if(this._isPlain) return String.fromCharCode(...decryptedChars)
+    else return String.fromCharCode(...decryptedChars.reverse())
   }
 }
 
